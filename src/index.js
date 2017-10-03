cbbab2cf1fcab96d058632c2564bc9339230bde3
yuxuan39 <kumo9102151@gmail.com>
2017-10-04 00:38:37 +0800

    get userdata

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import './styles/style.scss'
 
+const searchBtn = document.getElementById('search-btn')
+const searchInput = document.getElementById('search-bar')
+
+searchBtn.addEventListener('click', evt => {
+  const value = searchInput.value
+  getUser(value).then(data => {
+      console.log('data', data)
+  })
+  .catch(err => console.log('err', err))
+})
+
+const HOST = 'https://api.github.com/search/users'
+
+const getUser = value => {
+  return fetch(`${HOST}?q=${value}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    //   'Access-Control-Allow-Origin': '*'
+    }
+  }).then(res => {
+    return res.json()
+  })
+}
