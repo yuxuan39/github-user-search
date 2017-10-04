5339a1375ca82d18e2dbadfb1aa937eb39299896
yuxuan39 <kumo9102151@gmail.com>
2017-10-04 23:48:42 +0800

    getdata

--- src/index.js
+++ src/index.js
@@ -5,20 +5,28 @@ const searchInput = document.getElementById('search-bar')
 
 searchBtn.addEventListener('click', evt => {
   const value = searchInput.value
-  getUser(value).then(data => {
-      console.log('data', data)
-  })
-  .catch(err => console.log('err', err))
+  getUser(value)
+    .then(rawData => {
+      const data = rawData.items
+      console.log(data[0])
+      const getData = data.map(mainData => {
+        return {
+          avatar_url: mainData.avatar_url,
+          following_url: mainData.following_url
+        }
+      })
+      console.log(getData)
+    })
+    .catch(err => console.log('err', err))
 })
 
 const HOST = 'https://api.github.com/search/users'
 
 const getUser = value => {
-  return fetch(`${HOST}?q=${value}`, {
+  return fetch(`${HOST}?q=${value}&per_page=50&page=1`, {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json',
-    //   'Access-Control-Allow-Origin': '*'
+      'Content-Type': 'application/json'
     }
   }).then(res => {
     return res.json()
