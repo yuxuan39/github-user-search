817b64e606911f4d62ac2335facc5b6c846745f7
yuxuan39 <kumo9102151@gmail.com>
2017-10-12 19:17:20 +0800

    get next page

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ const searchInput = document.getElementById('search-bar')
 // const photo = document.getElementById('photo')
 // const following = document.getElementById('following')
 const user = document.getElementById('user')
-const load = document.getElementById('load-btn')
+const loadBtn = document.getElementById('load-btn')
 
 searchBtn.addEventListener('click', evt => {
+  // if(container){
+  //   user.removeChild(container)
+  // }
   const value = searchInput.value
   getUser(value)
     .then(rawData => {
       const data = rawData.items
-      // console.log(data[0])
       const getData = data
         .map(mainData => {
           return {
@@ -34,7 +36,38 @@ searchBtn.addEventListener('click', evt => {
           container.appendChild(img)
           user.appendChild(container)
         })
-      load.style.visibility = 'visible'
+      loadBtn.style.visibility = 'visible'
+      // userdata.innerText = 'getData[0].login'
+      // photo.innerHTML = '<a href="#"><img src="mdn-logo-sm.png" alt="MDN"></a>'
+    })
+    .catch(err => console.log('err', err))
+})
+
+loadBtn.addEventListener('click', evt => {
+  const value = searchInput.value
+  nextUserPage(value)
+    .then(rawData => {
+      const data = rawData.items
+      const getData = data
+        .map(mainData => {
+          return {
+            login: mainData.login,
+            avatar_url: mainData.avatar_url
+          }
+        })
+        .map(data => {
+          let container = document.createElement('div')
+          container.classList = 'container'
+          let p = document.createElement('p')
+          p.innerText = data.login
+
+          let img = document.createElement('img')
+          let url = data.avatar_url
+          img.src = url
+          container.appendChild(p)
+          container.appendChild(img)
+          user.appendChild(container)
+        })
       // userdata.innerText = 'getData[0].login'
       // photo.innerHTML = '<a href="#"><img src="mdn-logo-sm.png" alt="MDN"></a>'
     })
@@ -45,6 +78,21 @@ const HOST = 'https://api.github.com/search/users'
 
 const getUser = value => {
   return fetch(`${HOST}?q=${value}&per_page=48&page=1`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => {
+    return res.json()
+  })
+}
+
+let renderPage = 1
+
+const nextUserPage = value => {
+  renderPage = renderPage + 1
+  console.log(renderPage)
+  return fetch(`${HOST}?q=${value}&per_page=48&page=${renderPage}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
