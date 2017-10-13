ee183f87b964fbd18d2ec7fe8a66a699bf63758a
yuxuan39 <kumo9102151@gmail.com>
2017-10-13 20:57:52 +0800

    change bgc and add img href

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import './styles/style.scss'
 
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-bar')
-// const username = document.getElementById('username')
-// const photo = document.getElementById('photo')
-// const following = document.getElementById('following')
 const user = document.getElementById('user')
 const loadBtn = document.getElementById('load-btn')
+const loader = document.getElementById('loader')
 
 const clearUsers = () => {
   user.innerHTML = ''
 }
 
 searchBtn.addEventListener('click', evt => {
+  loadBtn.style.visibility = 'none'
   renderUsers()
 })
 
 document.body.addEventListener('keydown', evt => {
+  loadBtn.style.visibility = 'none'
   if (evt.keyCode === 13) {
     renderUsers()
   }
@@ -24,26 +24,43 @@ document.body.addEventListener('keydown', evt => {
 
 const renderUsers = () => {
   clearUsers()
-
+  loader.style.display = 'block'
   const value = searchInput.value
   getUsers(value)
     .then(rawData => {
+      if (rawData.total_count === 0) {
+        loadBtn.style.visibility = 'none'
+        loader.style.display = 'none'
+        console.log('not found')
+        let warn = document.createElement('p')
+        warn.innerText = 'Not Found'
+        warn.classList = 'warn'
+      }
+      if (rawData.total_count >= 24) {
+        setTimeout(function() {
+          loadBtn.style.visibility = 'visible'
+        }, 1500)
+      }
       const data = rawData.items
       const getData = data
         .map(mainData => {
           return {
             login: mainData.login,
-            avatar_url: mainData.avatar_url
+            avatar_url: mainData.avatar_url,
+            html_url: mainData.html_url
           }
         })
         .map(aUser => {
+          loader.style.display = 'none'
           renderAUser(aUser)
         })
-      loadBtn.style.visibility = 'visible'
       // userdata.innerText = 'getData[0].login'
       // photo.innerHTML = '<a href="#"><img src="mdn-logo-sm.png" alt="MDN"></a>'
     })
-    .catch(err => console.log('err', err))
+    .catch(err => {
+      loader.style.display = 'none'
+      console.log('err', err)
+    })
 }
 
 const renderAUser = aUser => {
@@ -52,11 +69,15 @@ const renderAUser = aUser => {
   let p = document.createElement('p')
   p.innerText = aUser.login
 
+  let a = document.createElement('a')
+  a.setAttribute('href', aUser.html_url)
+  a.setAttribute('target', '_blank')
   let img = document.createElement('img')
   let url = aUser.avatar_url
   img.src = url
+  a.appendChild(img)
   container.appendChild(p)
-  container.appendChild(img)
+  container.appendChild(a)
   user.appendChild(container)
 }
 
@@ -94,13 +115,16 @@ loadBtn.addEventListener('click', evt => {
 const HOST = 'https://api.github.com/search/users'
 
 const getUsers = value => {
-  const url = `${HOST}?q=${value}+type:user+in:login&per_page=48&page=1`
+  const url = `${HOST}?q=${value}+type:user+in:login&per_page=24&page=1`
   return fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     }
   }).then(res => {
+    if (res.status >= 300) {
+      alert('Oops! There is something wrong.')
+    }
     return res.json()
   })
 }
@@ -110,7 +134,7 @@ let renderPage = 1
 const nextUserPage = value => {
   renderPage = renderPage + 1
   console.log(renderPage)
-  return fetch(`${HOST}?q=${value}&per_page=48&page=${renderPage}`, {
+  return fetch(`${HOST}?q=${value}&per_page=24&page=${renderPage}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
