a8e1538c826b422427048896ab7d7d5099acc94d
yuxuan39 <kumo9102151@gmail.com>
2017-10-14 17:01:23 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -6,8 +6,26 @@ const user = document.getElementById('user')
 const loadBtn = document.getElementById('load-btn')
 const loader = document.querySelector('.loader')
 
-const clearUsers = () => {
-  user.innerHTML = ''  
+const resetPage = () => {
+  user.innerHTML = ''
+  hiddenLoader()
+  hiddenButton()
+}
+
+const showLoader = () => {
+  loader.style.visibility = 'visible'
+}
+
+const hiddenLoader = () => {
+  loader.style.visibility = 'hidden'
+}
+
+const showButton = () => {
+  loadBtn.style.visibility = 'visible'
+}
+
+const hiddenButton = () => {
+  loadBtn.style.visibility = 'hidden'
 }
 
 searchBtn.addEventListener('click', evt => {
@@ -20,59 +38,59 @@ document.body.addEventListener('keydown', evt => {
   }
 })
 
+const render404 = () => {
+  let warn = document.createElement('p')
+  warn.innerText = 'Not Found'
+  warn.classList = 'warn'
+  user.appendChild(warn)
+}
+
+const normalizeUsers = (users = []) =>
+  users.map(mainData => {
+    return {
+      login: mainData.login,
+      avatar_url: mainData.avatar_url,
+      html_url: mainData.html_url
+    }
+  })
+
 const renderUsers = () => {
-  clearUsers()
-  loader.style.visibility = 'visible'
+  resetPage()
+  showLoader()
   const value = searchInput.value
   getUsers(value)
     .then(rawData => {
       if (rawData.total_count === 0) {
-        loadBtn.style.visibility = 'hidden'
-        loader.style.visibility = 'hidden'
-        console.log('not found')
-        let warn = document.createElement('p')
-        warn.innerText = 'Not Found'
-        warn.classList = 'warn'
-        user.appendChild(warn)
+        resetPage()
+        render404()
       }
+      const users = rawData.items
+      normalizeUsers(users).map(userData => {
+        hiddenLoader()
+        renderUserData(userData)
+      })
+
       if (rawData.total_count >= 24) {
-        setTimeout(function() {
-          loadBtn.style.visibility = 'visible'
-        }, 1200)
+        showButton()
       }
-      const data = rawData.items
-      const getData = data
-        .map(mainData => {
-          return {
-            login: mainData.login,
-            avatar_url: mainData.avatar_url,
-            html_url: mainData.html_url
-          }
-        })
-        .map(aUser => {
-          loader.style.visibility = 'hidden'
-          renderAUser(aUser)
-        })
-      // userdata.innerText = 'getData[0].login'
-      // photo.innerHTML = '<a href="#"><img src="mdn-logo-sm.png" alt="MDN"></a>'
     })
     .catch(err => {
-      loader.style.visibility = 'none'
+      hiddenLoader()
       console.log('err', err)
     })
 }
 
-const renderAUser = aUser => {
+const renderUserData = userData => {
   let container = document.createElement('div')
   container.classList = 'container'
   let p = document.createElement('p')
-  p.innerText = aUser.login
+  p.innerText = userData.login
 
   let a = document.createElement('a')
-  a.setAttribute('href', aUser.html_url)
+  a.setAttribute('href', userData.html_url)
   a.setAttribute('target', '_blank')
   let img = document.createElement('img')
-  let url = aUser.avatar_url
+  let url = userData.avatar_url
   img.src = url
   a.appendChild(img)
   container.appendChild(p)
@@ -81,9 +99,9 @@ const renderAUser = aUser => {
 }
 
 loadBtn.addEventListener('click', evt => {
-  loader.style.visibility = 'visible'
+  showLoader()
   const value = searchInput.value
-  nextUserPage(value)
+  getNextUserPage(value)
     .then(rawData => {
       const data = rawData.items
       const getData = data
@@ -93,34 +111,21 @@ loadBtn.addEventListener('click', evt => {
             avatar_url: mainData.avatar_url
           }
         })
-        .map(data => {
-          loader.style.visibility = 'hidden'
-          let container = document.createElement('div')
-          container.classList = 'container'
-          let p = document.createElement('p')
-          p.innerText = data.login
-
-          let img = document.createElement('img')
-          let url = data.avatar_url
-          img.src = url
-          container.appendChild(p)
-          container.appendChild(img)
-          user.appendChild(container)
+        .map(userData => {
+          hiddenLoader()
+          renderUserData(userData)
         })
-      // userdata.innerText = 'getData[0].login'
-      // photo.innerHTML = '<a href="#"><img src="mdn-logo-sm.png" alt="MDN"></a>'
     })
     .catch(err => {
-      loader.style.visibility = 'hidden'
+      hiddenLoader()
       console.log('err', err)
     })
 })
 
-const HOST = 'https://api.github.com/search/users'
+const HOST = 'https://api.github.com'
 
-const getUsers = value => {
-  const url = `${HOST}?q=${value}+type:user+in:login&per_page=24&page=1`
-  return fetch(url, {
+const get = (url, query) => {
+  return fetch(HOST + url + '?' + query, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -128,22 +133,32 @@ const getUsers = value => {
   }).then(res => {
     if (res.status >= 300) {
       alert('Oops! There is something wrong.')
+      throw new Error('request failed')
     }
     return res.json()
   })
 }
 
-let renderPage = 1
-
-const nextUserPage = value => {
-  renderPage = renderPage + 1
-  console.log(renderPage)
-  return fetch(`${HOST}?q=${value}&per_page=24&page=${renderPage}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(res => {
-    return res.json()
-  })
+const getUsersByPage = (inputText, page) => {
+  return get(
+    '/search/users',
+    `q=${inputText}+type:user+in:login&per_page=24&page=${page}`
+  )
 }
+
+const getUsers = inputText => getUsersByPage(inputText, 1)
+
+let currentPage = 1
+const getNextUserPage = inputText => {
+  currentPage = currentPage + 1
+  const nextPage = currentPage
+  return getUsersByPage(inputText, nextPage)
+}
+
+// const getUsers = value => {
+//   const url = `${HOST}?q=${value}+type:user+in:login&per_page=24&page=1`
+//   return fetch(url, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
